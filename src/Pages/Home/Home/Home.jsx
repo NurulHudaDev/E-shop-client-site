2ac0e2bcf1033c349ae1e552338ac3af3655cc1d
NurulHudaDev <nurulhuda22dev@gmail.com>
2024-02-27 14:00:54 +0600

    products

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import AllProducts from '../AllProducts/AllProducts';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <AllProducts></AllProducts>
         </div>
     );
 };
